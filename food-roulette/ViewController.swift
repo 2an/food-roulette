43d334386236fafb5897333b89ba918194509a4f
Tuan Ngo <tuan.ngo@continental-corporation.com>
2018-12-04 13:09:15 +0100

    Initial Commit

---
+++ food-roulette/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  food-roulette
+//
+//  Created by Tuan Ngo on 04.12.18.
+//  Copyright © 2018 Tuan Ngo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
